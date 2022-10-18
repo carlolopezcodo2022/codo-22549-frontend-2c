0ba453a81f42238e591d2c639f8deb83a50cbb54
carlos horacio lopez <krloss@gmail.com>
2022-10-18 20:29:43 -0300

    clase21

--- clase20/index.js
+++ clase20/index.js
@@ -3,7 +3,8 @@ function buscar(clave) {
 
     //data.js > USER_DATA.data
     //funciones flecha / arrow function
-    const usersFiltered = USERS_DATA.data.filter(user => user.first_name.includes(clave) || user.last_name.includes(clave));
+    const usersFiltered = USERS_DATA.data
+    .filter(user => user.first_name.toLowerCase().includes(clave.toLowerCase()) || user.last_name.toLowerCase().includes(clave.toLowerCase()));
 
     const htmlUsers = Users(usersFiltered);
     document.getElementById('users').innerHTML = htmlUsers;
@@ -15,11 +16,30 @@ function render() {
 
     //usar al componente Navbar
     const htmlNavbar = Navbar('Mi primer pseudocomponente',buscar);
-    document.getElementById('navbar').innerHTML = htmlNavbar;
+    renderComponent('navbar',htmlNavbar);
+ 
+    search(1);
+}
 
+function search(page) {
     //ahora invoco a 
-    const htmlUsers = Users(USERS_DATA.data);
-    document.getElementById('users').innerHTML = htmlUsers;
+    
+    const url = ` https://reqres.in/api/users${page >=1 ? `?page=${page}` : ''}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const htmlUsers = Users(data.data);
+            renderComponent('users',htmlUsers);
+            
+            //ahora "cargo" el componente Paginator()
+            const htmlPaginator = Paginator(data);
+            renderComponent('paginator',htmlPaginator);
+        }
+    );
+}
+
+function renderComponent(id, htmlComponent) {
+    document.getElementById(id).innerHTML = htmlComponent;
 }
 
 //invocar?
