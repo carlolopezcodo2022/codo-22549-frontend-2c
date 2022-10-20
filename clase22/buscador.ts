aec6ba29372f399948c0a4ecd04a6a26a31f60c5
carlos horacio lopez <krloss@gmail.com>
2022-10-20 19:55:30 -0300

    ts

---
+++ clase22/buscador.ts
@@ -0,0 +1,41 @@
+/*ts superspet de js*/
+/*O.O.P*/
+/*
+    clases,interfaces,clases abstractas    
+ */
+
+/*clase*/
+class Articulo {
+  titulo:string; 
+  autor:string;
+  precio:number;
+}
+
+class Buscador {
+    //atributos
+    claveBusqueda: string;
+    articulos: Articulo[];
+
+    //metodos
+    buscar(): void {
+        console.log(`buscando con la clave ${this.claveBusqueda}`);
+    }
+}
+
+//definir un tipo de objeto
+interface Producto {
+    id: number,
+    nombre: string,
+}
+
+function comprar(prod:Producto): void{
+    
+}
+
+//invoke
+const unP:Producto = {
+    id:1,
+    nombre:'papel'
+};
+
+comprar(unP);
